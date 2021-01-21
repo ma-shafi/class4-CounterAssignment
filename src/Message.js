15c30fc1d34bbf479ff4d4334aeae738abab2bba
mashafi <mashafi.tech@gmail.com>
2021-01-21 23:40:34 +0500

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Message(props){
-    return <h1>You Counter Variable is : {props.counter}</h1>
+    return <h1>Your Counter Variable is : {props.counter}</h1>
 };
 
 export default Message;
