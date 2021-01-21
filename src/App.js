8aaa7832ed9f18e7c2acccc9cbf372b5b54e788f
mashafi <mashafi.tech@gmail.com>
2021-01-21 23:25:36 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Message from './Message.js'
+ 
+function App(){
+  let [count, setCount] = useState(0);
+  let [isDay, setDay] = useState(false);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className={`box ${isDay ? 'dayLight' : ''}`}>
+    <h1>Have A Good {isDay ? 'Morning' : 'Night'}</h1>
+    <Message counter={count} />
+    <br />
+    <button onClick={()=> setCount(++count)}>Update Counter </button>
+    <button onClick={()=> setDay(!isDay)}>Set Day</button>
     </div>
-  );
-}
+    )
+  };
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Message(props){
+    return <h1>You Counter Variable is : {props.counter}</h1>
+};
+
+export default Message;
